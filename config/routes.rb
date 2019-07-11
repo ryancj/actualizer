48cb2e28cced5009d37324580568fdcb858d67d0
Dohoon Kim <dohoonk.smg@gmail.com>
2019-07-10 18:58:40 -0700

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root "static#index"
-
-  resources :components
-=======
   root "components#index"
 
 
@@ -13,5 +8,4 @@ Rails.application.routes.draw do
   get '/show' => "static#show"
   get "/example" => "static#index"
   
->>>>>>> 4b10b65baae9c9751a2b5940537c24a32fed7608
 end
